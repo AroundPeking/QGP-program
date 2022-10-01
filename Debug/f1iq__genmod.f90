da83586fcff0cea4f1075e3a511934c1ff32454d
Huanjing Gong <gonghuanjing@qq.com>
2022-10-01 11:51:30 +0800

    store all

---
+++ Debug/f1iq__genmod.f90
@@ -0,0 +1,12 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Oct  1 11:46:19 2022
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE F1IQ__genmod
+          INTERFACE 
+            FUNCTION F1IQ(Q,IDP)
+              REAL(KIND=8) :: Q
+              INTEGER(KIND=4) :: IDP
+              REAL(KIND=8) :: F1IQ
+            END FUNCTION F1IQ
+          END INTERFACE 
+        END MODULE F1IQ__genmod
---
+++ Debug/thermal__genmod.f90
@@ -0,0 +1,12 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Oct  1 11:46:19 2022
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE THERMAL__genmod
+          INTERFACE 
+            FUNCTION THERMAL(PT,IDP)
+              REAL(KIND=8) :: PT
+              INTEGER(KIND=4) :: IDP
+              REAL(KIND=8) :: THERMAL
+            END FUNCTION THERMAL
+          END INTERFACE 
+        END MODULE THERMAL__genmod
